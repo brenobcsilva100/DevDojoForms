5fac6b560175485a2dbbc92449aef44c8d5f62cc
brenobcsilva100 <brenobcsilva@gmail.com>
2023-09-01 15:31:58 -0300

    Criando arquivo de conexao com mysql

---
+++ src/main/java/br/com/devdojo/examgenerator/DevDojoWillianApplication.java
@@ -0,0 +1,13 @@
+package br.com.devdojo.examgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevDojoWillianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevDojoWillianApplication.class, args);
+    }
+
+}
